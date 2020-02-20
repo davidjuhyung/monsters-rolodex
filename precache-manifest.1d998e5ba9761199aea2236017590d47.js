6d5d21768c2db11efd490abcd47b95d2b536129b
David <j626kim@uwaterloo.ca>
2020-02-20 03:52:21 -0500

    Updates

---
+++ precache-manifest.1d998e5ba9761199aea2236017590d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eda04b4fe12458efc6fda1672f08047",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4edcda8ed49ea4a7fceb",
+    "url": "/monsters-rolodex/static/css/main.bb841f6e.chunk.css"
+  },
+  {
+    "revision": "7f22acf8ea8a1a37b289",
+    "url": "/monsters-rolodex/static/js/2.6f41c62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.6f41c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edcda8ed49ea4a7fceb",
+    "url": "/monsters-rolodex/static/js/main.f8c828f0.chunk.js"
+  },
+  {
+    "revision": "c1e4297d3516a0fe750b",
+    "url": "/monsters-rolodex/static/js/runtime-main.12f886c8.js"
+  }
+]);
