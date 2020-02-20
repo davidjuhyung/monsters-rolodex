6e6ce4e612f360983597a14e84ea771c2e902ee5
David <j626kim@uwaterloo.ca>
2020-02-20 03:23:47 -0500

    npx create react app init

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+class App extends Component {
+	state = {
+		isFetching: true,
+		monsters: [],
+		searchField: '',
+		title: ''
+	};
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ monsters: users, isFetching: false }));
+	}
+
+	handleChange = e => {
+		this.setState({ searchField: e.target.value, title: e.target.value });
+	};
+
+	render() {
+		const { monsters, searchField, title } = this.state;
+
+		const filteredMonsters = monsters.filter(monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
+		return (
+			<div className="App">
+				<h1>{title}</h1>
+				<SearchBox
+					placeholder="search monsters..."
+					handleChange={this.handleChange}
+				></SearchBox>
+				<CardList
+					monsters={filteredMonsters}
+					isFetching={this.state.isFetching}
+				></CardList>
+			</div>
+		);
+	}
+}
+
+export default App;
